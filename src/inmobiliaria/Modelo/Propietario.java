e1db0cda2338e2559826ecd9f70c75b5e921264d
FranciscoCordoba <franciscoleon45@gmail.com>
2022-07-04 18:05:59 -0300

    Clase propietario

--- src/inmobiliaria/Modelo/Propietario.java
+++ src/inmobiliaria/Modelo/Propietario.java
@@ -1,7 +1,121 @@
 package inmobiliaria.Modelo;
 
 public class Propietario {
+    //A
+    private int id;
+    private String nombre;
+    private String apellido;
+    private int dni;
+    private String domicilio;
+    private int telefono;
+    private int propiedades;
+    
+    //C
+    public Propietario() {
+    }
+
+    public Propietario(String nombre, String apellido, int dni, String domicilio, int telefono, int propiedades) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.domicilio = domicilio;
+        this.telefono = telefono;
+        this.propiedades = propiedades;
+    }
+
+    public Propietario(int id, String nombre, String apellido, int dni, String domicilio, int telefono, int propiedades) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.domicilio = domicilio;
+        this.telefono = telefono;
+        this.propiedades = propiedades;
+    }
+    
+    //M
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getPropiedades() {
+        return propiedades;
+    }
+
+    public void setPropiedades(int propiedades) {
+        this.propiedades = propiedades;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 47 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Propietario other = (Propietario) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
     
     
+
+    @Override
+    public String toString() {
+        return "Propietario{" + "id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + ", domicilio=" + domicilio + ", telefono=" + telefono + ", propiedades=" + propiedades + '}';
+    }
     
 }

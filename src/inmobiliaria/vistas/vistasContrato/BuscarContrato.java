8ddf7b10409a688ac4cf1d6e858b0770b44d90ca
Nica <nicanorsuaressanchez@gmail.com>
2022-07-15 13:28:39 -0300

    BUSCAR CONTRATOOOOO
    VAMO LOCO TODAVIAA

--- src/inmobiliaria/vistas/vistasContrato/BuscarContrato.java
+++ src/inmobiliaria/vistas/vistasContrato/BuscarContrato.java
@@ -24,6 +24,17 @@ public class BuscarContrato extends javax.swing.JPanel {
 
     public BuscarContrato() {
 	initComponents();
+	conexion = new Conexion();
+	propietarioData = new PropietarioData(conexion);
+	inquilinoData = new InquilinoData(conexion);
+	contratoData = new ContratoData(conexion);
+	propiedadData = new PropiedadData(conexion);
+	jtfInquilino.setEnabled(false);
+	jbtnLimpiar.setEnabled(false);
+	jbtnContrato.setEnabled(false);
+	jtfPropietario.setEnabled(false);
+	modelo = new DefaultTableModel();
+	armarCabeceraTabla();
     }
 
     @SuppressWarnings("unchecked")
@@ -32,7 +43,7 @@ public class BuscarContrato extends javax.swing.JPanel {
 
         jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        jtListaContrato = new javax.swing.JTable();
         jtfInquilino = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
         jtfDniInq = new javax.swing.JTextField();
@@ -43,12 +54,12 @@ public class BuscarContrato extends javax.swing.JPanel {
         jtfPropietario = new javax.swing.JTextField();
         jLabel9 = new javax.swing.JLabel();
         jbtnBuscarPro = new javax.swing.JButton();
-        jRadioButton1 = new javax.swing.JRadioButton();
-        jRadioButton2 = new javax.swing.JRadioButton();
+        vigentes = new javax.swing.JRadioButton();
+        noVigentes = new javax.swing.JRadioButton();
         jbtnContrato = new javax.swing.JButton();
         jbtnLimpiar = new javax.swing.JButton();
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        jtListaContrato.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -59,7 +70,7 @@ public class BuscarContrato extends javax.swing.JPanel {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(jtListaContrato);
 
         jtfInquilino.setBackground(new java.awt.Color(217, 217, 217));
 
@@ -111,14 +122,19 @@ public class BuscarContrato extends javax.swing.JPanel {
             }
         });
 
-        jRadioButton1.setText("Vigentes");
-        jRadioButton1.addActionListener(new java.awt.event.ActionListener() {
+        vigentes.setText("Vigentes");
+        vigentes.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jRadioButton1ActionPerformed(evt);
+                vigentesActionPerformed(evt);
             }
         });
 
-        jRadioButton2.setText("No vigentes");
+        noVigentes.setText("No vigentes");
+        noVigentes.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                noVigentesActionPerformed(evt);
+            }
+        });
 
         jbtnContrato.setBackground(new java.awt.Color(0, 63, 121));
         jbtnContrato.setText("Ver Contratos");
@@ -164,7 +180,7 @@ public class BuscarContrato extends javax.swing.JPanel {
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(jLabel9)
                                     .addComponent(jtfPropietario, javax.swing.GroupLayout.PREFERRED_SIZE, 213, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jRadioButton1, javax.swing.GroupLayout.Alignment.TRAILING))
+                                    .addComponent(vigentes, javax.swing.GroupLayout.Alignment.TRAILING))
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addGroup(jPanel1Layout.createSequentialGroup()
                                         .addGap(32, 32, 32)
@@ -175,7 +191,7 @@ public class BuscarContrato extends javax.swing.JPanel {
                                         .addComponent(jbtnBuscarPro))
                                     .addGroup(jPanel1Layout.createSequentialGroup()
                                         .addGap(18, 18, 18)
-                                        .addComponent(jRadioButton2))))))
+                                        .addComponent(noVigentes))))))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(150, 150, 150)
                         .addComponent(jbtnContrato)
@@ -206,8 +222,8 @@ public class BuscarContrato extends javax.swing.JPanel {
                     .addComponent(jtfPropietario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(29, 29, 29)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jRadioButton1)
-                    .addComponent(jRadioButton2))
+                    .addComponent(vigentes)
+                    .addComponent(noVigentes))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
@@ -284,10 +300,20 @@ public class BuscarContrato extends javax.swing.JPanel {
 	    for (Contrato cont : listacontratos) {
 		modelo.addRow(new Object[]{cont.getPropiedadContrato().getIdInmueble(), (cont.getPropietarioContrato().getApellido() + ", " + cont.getPropietarioContrato().getNombre()), (cont.getInquilinoContrato().getApellido() + ", " + cont.getInquilinoContrato().getNombre()), cont.getPropiedadContrato().getDireccion(), cont.getMonto(), cont.getFechaInicio(), cont.getFechaFin()});
 	    }
-	} else {
+	} else if (jbtnBuscarPro.isEnabled()) {
 	    long dniInq = Long.parseLong(jtfDniPro.getText());
 	    Propietario propie = propietarioData.obtenerPropietarioPorDni(dniInq);
 	    listacontratos = contratoData.contratosXPropietario(propie.getId());
+	    for (Contrato cont : listacontratos) {
+		modelo.addRow(new Object[]{cont.getPropiedadContrato().getIdInmueble(), (cont.getPropietarioContrato().getApellido() + ", " + cont.getPropietarioContrato().getNombre()), (cont.getInquilinoContrato().getApellido() + ", " + cont.getInquilinoContrato().getNombre()), cont.getPropiedadContrato().getDireccion(), cont.getMonto(), cont.getFechaInicio(), cont.getFechaFin(), cont.isActivo()});
+	    }
+	} else if (vigentes.isSelected()) {
+	    listacontratos = contratoData.mostrarContratosVigentes();
+	    for (Contrato cont : listacontratos) {
+		modelo.addRow(new Object[]{cont.getPropiedadContrato().getIdInmueble(), (cont.getPropietarioContrato().getApellido() + ", " + cont.getPropietarioContrato().getNombre()), (cont.getInquilinoContrato().getApellido() + ", " + cont.getInquilinoContrato().getNombre()), cont.getPropiedadContrato().getDireccion(), cont.getMonto(), cont.getFechaInicio(), cont.getFechaFin(), cont.isActivo()});
+	    }
+	} else {
+	    listacontratos = contratoData.mostrarVencidos();
 	    for (Contrato cont : listacontratos) {
 		modelo.addRow(new Object[]{cont.getPropiedadContrato().getIdInmueble(), (cont.getPropietarioContrato().getApellido() + ", " + cont.getPropietarioContrato().getNombre()), (cont.getInquilinoContrato().getApellido() + ", " + cont.getInquilinoContrato().getNombre()), cont.getPropiedadContrato().getDireccion(), cont.getMonto(), cont.getFechaInicio(), cont.getFechaFin(), cont.isActivo()});
 	    }
@@ -298,26 +324,48 @@ public class BuscarContrato extends javax.swing.JPanel {
 	limpiarCampos();
     }//GEN-LAST:event_jbtnLimpiarActionPerformed
 
-    private void jRadioButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton1ActionPerformed
-	// TODO add your handling code here:
-    }//GEN-LAST:event_jRadioButton1ActionPerformed
+    private void vigentesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_vigentesActionPerformed
+
+	noVigentes.setSelected(false);
+	jbtnContrato.setEnabled(true);
+	jbtnLimpiar.setEnabled(true);
+	jtfDniInq.setEnabled(false);
+	jbtnBuscarInq.setEnabled(false);
+	jtfDniPro.setEnabled(false);
+	jbtnBuscarPro.setEnabled(false);
+
+    }//GEN-LAST:event_vigentesActionPerformed
+
+    private void noVigentesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_noVigentesActionPerformed
+
+	vigentes.setSelected(false);
+	jbtnContrato.setEnabled(true);
+	jbtnLimpiar.setEnabled(true);
+	jtfDniInq.setEnabled(false);
+	jbtnBuscarInq.setEnabled(false);
+	jtfDniPro.setEnabled(false);
+	jbtnBuscarPro.setEnabled(false);
+
+    }//GEN-LAST:event_noVigentesActionPerformed
 
     private void limpiarCampos() {
-        jtfDniInq.setText("");
-        jtfDniPro.setText("");
-        jtfInquilino.setText("");
-        jtfPropietario.setText("");
+	jtfDniInq.setText("");
+	jtfDniPro.setText("");
+	jtfInquilino.setText("");
+	jtfPropietario.setText("");
 
-        jbtnContrato.setEnabled(false);
-        jbtnBuscarPro.setEnabled(true);
-        jbtnBuscarInq.setEnabled(true);
-        jtfPropietario.setEnabled(false);
-        jtfInquilino.setEnabled(false);
-        jtfDniPro.setEnabled(true);
-        jtfDniInq.setEnabled(true);
-        jbtnLimpiar.setEnabled(false);
-
-        borrarFilasTabla();
+	jbtnContrato.setEnabled(false);
+	jbtnBuscarPro.setEnabled(true);
+	jbtnBuscarInq.setEnabled(true);
+	jtfPropietario.setEnabled(false);
+	jtfInquilino.setEnabled(false);
+	jtfDniPro.setEnabled(true);
+	jtfDniInq.setEnabled(true);
+	jbtnLimpiar.setEnabled(false);
+	vigentes.setSelected(false);
+	noVigentes.setSelected(false);
+	
+	borrarFilasTabla();
     }
 
     private void borrarFilasTabla() {
@@ -332,23 +380,39 @@ public class BuscarContrato extends javax.swing.JPanel {
 
     }
 
+    private void armarCabeceraTabla() {
+	ArrayList<Object> columnas = new ArrayList<Object>();
+	columnas.add("Direccion ");
+	columnas.add("Altura");
+	columnas.add("Tipo de Inmueble");
+	columnas.add("Ambientes");
+	columnas.add("Zona");
+	columnas.add("Superficie");
+	columnas.add("Precio");
+
+	for (Object it : columnas) {
+	    modelo.addColumn(it);
+	}
+	jtListaContrato.setModel(modelo);
+    }
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JRadioButton jRadioButton1;
-    private javax.swing.JRadioButton jRadioButton2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
     private javax.swing.JButton jbtnBuscarInq;
     private javax.swing.JButton jbtnBuscarPro;
     private javax.swing.JButton jbtnContrato;
     private javax.swing.JButton jbtnLimpiar;
+    private javax.swing.JTable jtListaContrato;
     private javax.swing.JTextField jtfDniInq;
     private javax.swing.JTextField jtfDniPro;
     private javax.swing.JTextField jtfInquilino;
     private javax.swing.JTextField jtfPropietario;
+    private javax.swing.JRadioButton noVigentes;
+    private javax.swing.JRadioButton vigentes;
     // End of variables declaration//GEN-END:variables
 }
